73d84cfc18239a2343c2f03654dc4101ef31676a
Pallab499 <sarkarpallab975@gmail.com>
2025-03-08 23:49:05 +0530

    updated

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -4,21 +4,18 @@ type Props = {}
 
 export default function HeroSection({}: Props) {
   return (
-    <div className="card" style={{width: "20rem"}}>
-  <img src="download (1).jpg" className="card-img-top rounded-lg p-4 mx-auto" alt="..."/>
-  <div className="card-body bg-blue-200 border border-blue-500 shadow-lg rounded-lg p-6 mx-2">
-    <h5 className="card-title transition duration-300 hover:bg-purple-500 hover:text-white rounded-lg p-2">Surfing Area</h5>
-    <p className="card-text shadow-md p-4 mt-4 rounded-lg p-2 bg-gradient-to-r from-pink-500 to-blue-500 text-white rounded-md">Here are the List of Upcoming Special events and Elections to be held.</p>
-    <ul className="list-group list-group-flush shadow-md p-4 mt-4 rounded-lg bg-blue-500 mx-auto">
-    <li className="list-group-item font-serif p-4 space-y-2 bg-gradient-to-r from-pink-500 to-blue-500 text-white rounded-lg">LIST OF UPCOMING EVENTS!</li>
-    <li className="list-group-item p-2 bg-gradient-to-r from-pink-500 to-blue-500 text-white rounded-md">1. General Elections</li>
-    <li className="list-group-item p-2 bg-gradient-to-r from-pink-500 to-blue-500 text-white rounded-md">2. College Fest</li>
-    </ul>
-  <div className="card-body">
-  <button className="flex-col btn btn-primary rounded my-4 mx-6 py-2 px-2 text-white bg-gradient-to-r from-blue-500 to-pink-500 font-semibold hover:from-pink-500 hover:to-blue-500 transition duration-300">Register</button>
-  <button className="flex-col btn btn-primary rounded bg-gradient-to-r from-pink-500 to-blue-500 text-white px-2 py-2 my-4 mx-6 rounded-lg font-semibold hover:from-blue-500 hover:to-pink-500 transition duration-300">Login</button>
-  </div>
-</div>
-</div>
+    
+<section className="dark:bg-gray-100 dark:text-gray-800 flex flex-col items-center justify-center h-screen">
+	<div className="container mx-auto flex flex-col items-center justify-center px-4 py-6 text-center md:py- 6 md:px-4 lg:px-4 xl:max-w-3xl">
+		<h1 className="text-4xl font-bold leading-none sm:text-4xl justify-center text-lg">Welcome To Our <span className='dark:text-violet-600'>Home</span> Page!
+		</h1>
+		<p className="px-8 mt-8 mb-12 text-lg">To Choose Your Own Voice. To choose Our Own Leader. We Are A Democracy, We Choose For Us!</p>
+		<div className="flex flex-wrap justify-center">
+			<button className="px-8 py-3 m-2 text-lg font-semibold rounded bg-gradient-to-r from-violet-500 to-blue-500 hover:from-blue-500 hover:to-violet-500 dark:text-gray-50">Login</button>
+			<button className="px-8 py-3 m-2 text-lg border rounded dark:text-black-900 dark:border-gray-300">Register</button>
+		</div>
+	</div>
+</section>
+
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import Navbar from "./components/Navbar";
 export default function () {
   return (
     <>
+    <section className="relative bg-C:\Users\PALLAB SARKAR\OneDrive\Pictures\Screenshots 1\fee53378a22c7d0c50faa150d23e3ffa1718944751.jpg bg-cover bg-center bg-no-repeat">
     <div className="flex flex-col bg-gradient-to-r from-gray-200 to-blue-300 gap-4 min-h-screen">
       <Navbar />
       <HeroSection/>
     </div>
+    </section>
   
     </>
   )
